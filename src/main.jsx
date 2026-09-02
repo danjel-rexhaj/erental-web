a536a4be8722caa6c6e936d6238f4c5cb08d8c68
Danjel Rexhaj <lelirexhaj@gmail.com>
2026-09-02 16:46:54 +0200

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { LangProvider } from './i18n.jsx'
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LangProvider>
       <App />
+      <Analytics />
     </LangProvider>
   </StrictMode>,
 )
